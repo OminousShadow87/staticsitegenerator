a7174a9a2406d8e9de7645bc835d0652be67423c
OminousShadow87 <brucewaynephil@gmail.com>
2025-01-03 09:59:22 -0800

    Ch4L3 quote, paragraph, code

--- mark2html.py
+++ mark2html.py
@@ -10,7 +10,9 @@ def markdown_to_html_node(markdown):
     for block in mkd_blocks:
         match(block_to_block_type(block)):
             case "code":
-                
+                code_leaf = LeafNode(tag = "code", value = block)
+                code_parent = ParentNode(tag = "pre", children = [code_leaf])
+                papa.children.append(code_parent)
             case "heading":
                 hash_count = len(block) - len(block.lstrip('#'))
                 heading_text = block[hash_count:].strip()
@@ -21,10 +23,13 @@ def markdown_to_html_node(markdown):
             case "unordered_list":
                 
             case "quote":
-                
+                quote_text = block.lstrip(">").strip()
+                quote_node = LeafNode(tag = "blockquote", value = quote_text)
+                papa.children.append(quote_node)
             case "paragraph":
-    
-
+                pg_text = block.strip()
+                pg_node = LeafNode(tag = "p", value = pg_text)
+                papa.children.append(pg_node)
 
     html_result = papa.to_html()
     return html_result
