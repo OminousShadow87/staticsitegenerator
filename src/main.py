6ac9cf0e97aa4bf57ce8a4489f27572d71256e91
OminousShadow87 <brucewaynephil@gmail.com>
2025-01-06 14:22:58 -0800

    Ch5 L1

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 from textnode import *
+import os
+import shutil
 
 
 def main():
-    bootytext = TextNode("get that booty", TextType.ITALIC_TEXT, "https://nationalguitaracademy.com/best-beginner-guitar/")
-    print(bootytext)
+    public_dir = "/home/breeze/workspace/github.com/breeze/staticsitegenerator/public"
+    static_dir = "/home/breeze/workspace/github.com/breeze/staticsitegenerator/static"
+    copy_directory(static_dir, public_dir)
+
+def copy_directory(current_dir, destination_dir):
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+    os.mkdir(destination_dir)
+    dir_list = os.listdir(current_dir)
+    for item in dir_list:
+        current_path = os.path.join(current_dir, item)
+        destination_path = os.path.join(destination_dir, item)
+        if os.path.isfile(current_path):
+            shutil.copy(current_path, destination_path)
+        elif os.path.isdir(current_path):
+            copy_directory(current_path, destination_path)     
 
 if __name__ == "__main__":
     main()
